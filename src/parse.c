3e5e9203da7706d08088acb6a029919415135c2f
s <ssoehnlen@protonmail.com>
2026-01-22 13:23:16 +0100

    debug

--- src/parse.c
+++ src/parse.c
@@ -75,7 +75,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
 
 }
 
-int create_db_header(int fd, struct dbheader_t **headerOut) {
+int create_db_header(struct dbheader_t **headerOut) {
 	struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
 	if (header == -1) {
 		printf("Malloc failed to create db header\n");
