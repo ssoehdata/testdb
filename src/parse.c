dc69ef8f80bcc130c259b5884c38c766201bc8c5
s <ssoehnlen@protonmail.com>
2026-01-22 13:36:10 +0100

    testing edits

--- src/parse.c
+++ src/parse.c
@@ -35,7 +35,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
 	}
 
 	struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
-	//if (header == -1) {
+	if (header == (void *)-1) {
 		printf("Malloc failed to create a db header\n");
 		return STATUS_ERROR;
 	}
@@ -75,11 +75,11 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
 
 }
 
-int create_db_header(struct dbheader_t **headerOut) {
+int create_db_header(/*int fd, */struct dbheader_t **headerOut) {
 	struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
 	//if (header == -1) {
-		printf("Malloc failed to create db header\n");
-		return STATUS_ERROR; 
+		//printf("Malloc failed to create db header\n");
+		//return STATUS_ERROR; 
 	}
 
 	header->version = 0x1;
